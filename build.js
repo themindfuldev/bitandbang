6c9909f1fe5823a9d5ef1504fd425ee21c0f1130
Tiago Romero <tiagoromerogarcia@gmail.com>
2024-10-23 13:01:01 -0700

    new version

--- build.js
+++ build.js
@@ -15,18 +15,16 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.yellowBright('Tiago Romero Garcia'),
+  name: chalk.yellowBright('Tiago Romero'),
   handle: chalk.yellow('themindfuldev'),
-  work: chalk.white('Tech Lead at ') + chalk.cyanBright('Pinterest'),
-  twitter: chalk.white('https://twitter.com/') + chalk.greenBright('themindfuldev'),
+  work: chalk.white('Software Engineer and Dog Daddy at ') + chalk.cyanBright('taking a sabbatical'),
   npm: chalk.white('https://npmjs.com/') + chalk.greenBright('~themindfuldev'),
   github: chalk.white('https://github.com/') + chalk.greenBright('themindfuldev'),
-  linkedin: chalk.white('https://linkedin.com/in/') + chalk.greenBright('themindfuldev'),
+  linkedin: chalk.white('https://linkedin.com/in/') + chalk.greenBright('tiagoromero'),
   dev: chalk.white('https://dev.to/') + chalk.greenBright('themindfuldev'),
-  web: chalk.white('https://') + chalk.redBright('tiagorg.com'),
+  web: chalk.white('https://') + chalk.redBright('tiagoromero.me'),
   npx: chalk.magentaBright('npx') + ' ' + chalk.magentaBright('themindfuldev'),
   labelWork: chalk.white.bold('       Work:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
   labelnpm: chalk.white.bold('        npm:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
@@ -39,7 +37,6 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
@@ -51,7 +48,6 @@ const carding = `${data.labelCard}  ${data.npx}`
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
